429700efacc4480b12eb9f2bdce5b97816a0d189
Skenks <84154852+Skenks@users.noreply.github.com>
2021-05-20 20:36:39 +0200

    Add files via upload
    -Basic citanje grafa  Cvor -> sljedbenici

--- GonzaloNavarro.py
+++ GonzaloNavarro.py
@@ -1,21 +1,44 @@
 import itertools
+import gfapy
 
 
-# Vertice of a graph
-class Vertice:
+# Node of a graph
+class Node:
     # list of predecessor nodes
     e = []
     # list of successor nodes
     succ = []
+    # name - number
+    name = ''
     # C value
     c1 = 0
     # C' value
     c2 = 0
     # char data of a node
     data = ''
+    # split = boolean
+    split = False
+    # split_list -> if len(data) > 1 -> split string into nodes
+    split_list = []
 
-    def __init__(self, e):
-        self.e.append(e)
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.succ = []
+        self.e = []
+        self.c1 = 0
+        self.c2 = 0
+        self.split = False
+        self.split_list = []
+
+    def set_e(self, value):
+        self.e.append(value)
+
+    def set_succ(self, value):
+        self.succ.append(value)
+
+    def set_name(self, value):
+        self.name = value
 
     def set_c1(self, value):
         self.c1 = value
@@ -25,6 +48,9 @@ class Vertice:
 
     def set_data(self, value):
         self.data = value
+
+    def set_split(self, value):
+        self.split = value
 
     def get_c1(self):
         return self.c1
@@ -38,10 +64,69 @@ class Vertice:
     def get_data(self):
         return self.data
 
+    def get_name(self):
+        return self.name
+
+    def get_split(self):
+        return self.split
+
+    def get_split_list(self):
+        return self.split_list
+
+    def __str__(self):
+
+        output_str = self.name
+        output_str += '-->'
+        for x in self.succ:
+            output_str += ";" + str(x)
+
+        return output_str
+
+
+def gfa():
+    # nikaj to ipak...
+    #
+    #
+    #
+    filepath = 'twopath.gfa'
+    g = gfapy.Gfa.from_file(filepath)
+    # print(str(g))
+    print(g.edge_names)
+
 
 def process_data():
     # data processing...
-    return {}, 'bbbb'
+    file_path = "twopath.gfa"
+    file = open(file_path, 'r')
+    all_lines = file.readlines()
+    graph = []
+    link = {}
+
+    for x in all_lines:
+        atr = x.strip().split()
+        # print(atr)
+
+        if atr[0] == "S":
+            node = Node(atr[1], atr[2])
+            node.set_data(atr[2])
+            graph.append(node)
+
+        if atr[0] == "L":
+            if atr[1] in link.keys():
+                if atr[3] not in link[atr[1]]:
+                    link[atr[1]].append(atr[3])
+            else:
+                link[atr[1]] = [atr[3]]
+    print(link)
+
+    for k, v in link.items():
+        # splitanje oblika 1 -> [
+        for value in v:
+            graph[int(k) - 1].set_succ(value)
+            n = graph[int(k) - 1]
+            print(n)
+
+    return graph, 'bbbb'
 
 
 # g(v,i) function
@@ -93,4 +178,11 @@ def main():
 
     graph, patt = process_data()
 
-    Search(graph, patt)
+    for ggg in graph:
+        print(ggg)
+        print()
+    # gfa()
+    # Search(graph, patt)
+
+
+main()
